360aa90b034c447e92fbccdc0286e3b601bac4d8
felip <felip@192.168.80.17>
2026-04-29 10:34:33 -0500

    NavigationMap grafo no dirigido con sobrecarga de addEdge

--- src/main/java/Modelo/NavigationMap.java
+++ src/main/java/Modelo/NavigationMap.java
@@ -5,7 +5,7 @@
 package Modelo;
 import java.util.*;
 /**
- ***
+ *
  * Representa el mapa del entorno interior como un grafo de nodos.
  * 
  * El mapa contiene nodos (posiciones) y aristas (conexiones entre posiciones).
